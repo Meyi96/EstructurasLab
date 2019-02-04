21dd7bb08be04ac820dbf24d0afcd53cd404f422
Your Name <you@example.com>
2019-02-03 22:00:10 -0500

    Interfaz configuracion botones

--- Lab1/src/Controller/CalculatorController.java
+++ Lab1/src/Controller/CalculatorController.java
@@ -14,154 +14,62 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
 public class CalculatorController  implements Initializable{
-
+	
 	@FXML
     private TextField x10;
-
     @FXML
     private TextField x9;
-
     @FXML
     private TextField x8;
-
     @FXML
     private TextField x7;
-
     @FXML
     private TextField x6;
-
     @FXML
     private TextField x5;
-
     @FXML
     private TextField x4;
-
     @FXML
     private TextField x3;
-
     @FXML
     private TextField x2;
-
     @FXML
     private TextField x1;
-
     @FXML
     private TextField x0;
+    @FXML
+    private ComboBox<String> SelecGrade;
     
-    private int current = 10;
+    private int limited;
+    private int current;
     private TextField[] textfield;
 
-    @FXML
-    private ComboBox<?> SelecGrade;
-
-    @FXML
-    void press0(ActionEvent event) {
-    	textfield[current].setText(textfield[current].getText()+"0");
-    }
-
-    @FXML
-    void press1(ActionEvent event) {
-    	textfield[current].setText(textfield[current].getText()+"1");
-    }
-
-    @FXML
-    void press2(ActionEvent event) {
-    	textfield[current].setText(textfield[current].getText()+"2");
-    }
-
-    @FXML
-    void press3(ActionEvent event) {
-    	textfield[current].setText(textfield[current].getText()+"3");
-    }
-
-    @FXML
-    void press4(ActionEvent event) {
-    	textfield[current].setText(textfield[current].getText()+"4");
-    }
-
-    @FXML
-    void press5(ActionEvent event) {
-    	textfield[current].setText(textfield[current].getText()+"5");
-    }
-
-    @FXML
-    void press6(ActionEvent event) {
-    	textfield[current].setText(textfield[current].getText()+"6");
-    }
-
-    @FXML
-    void press7(ActionEvent event) {
-    	textfield[current].setText(textfield[current].getText()+"7");
-    }
-
-    @FXML
-    void press8(ActionEvent event) {
-    	textfield[current].setText(textfield[current].getText()+"8");
-    }
-
-    @FXML
-    void press9(ActionEvent event) {
-    	textfield[current].setText(textfield[current].getText()+"9");
-    }
     
-    @FXML
-    void pressNegative(ActionEvent event) {
-    	textfield[current].setText("-");
-    }
 
     @FXML
-    void pressBefore(ActionEvent event) {
-    	if(current ==10) 
-    		current = 0;
-    	else
-    		current++;
-    	textfield[current].requestFocus();
-    }
-
+    void press0(ActionEvent event) {textfield[current].setText(textfield[current].getText()+"0");}
     @FXML
-    void pressCalculate(ActionEvent event) {
-    }
-
+    void press1(ActionEvent event) {textfield[current].setText(textfield[current].getText()+"1");}
     @FXML
-    void pressClean(ActionEvent event) {
-
-    }
-
+    void press2(ActionEvent event) {textfield[current].setText(textfield[current].getText()+"2");}
     @FXML
-    void pressDivide(ActionEvent event) {
-    	textfield[current].setText(textfield[current].getText()+"/");
-    }
-
+    void press3(ActionEvent event) {textfield[current].setText(textfield[current].getText()+"3");}
     @FXML
-    void pressNext(ActionEvent event) {
-    	if(current ==0) 
-    		current = 10;
-    	else
-    		current--;
-    	textfield[current].requestFocus();
-    		
-    }
-
+    void press4(ActionEvent event) {textfield[current].setText(textfield[current].getText()+"4");}
     @FXML
-    void pressPost(ActionEvent event) {
-
-    }
-
+    void press5(ActionEvent event) {textfield[current].setText(textfield[current].getText()+"5");}
     @FXML
-    void pressPre(ActionEvent event) {
-    	
-    }
-
+    void press6(ActionEvent event) {textfield[current].setText(textfield[current].getText()+"6");}
     @FXML
-    void pressRandom(ActionEvent event) {
-    	Platform.runLater(() -> {
-            if (!x8.isFocused()) {
-                x8.requestFocus();
-            }else
-            	x7.requestFocus();
-        });
-    }
-    
+    void press7(ActionEvent event) {textfield[current].setText(textfield[current].getText()+"7");}
+    @FXML
+    void press8(ActionEvent event) {textfield[current].setText(textfield[current].getText()+"8");}
+    @FXML
+    void press9(ActionEvent event) {textfield[current].setText(textfield[current].getText()+"9");}
+    @FXML
+    void pressDivide(ActionEvent event) {textfield[current].setText(textfield[current].getText()+"/");}
+    @FXML
+    void pressNegative(ActionEvent event) {textfield[current].setText("-");}
     @FXML
     void click0(MouseEvent event) {current =0;}
     @FXML
@@ -184,13 +92,69 @@ public class CalculatorController  implements Initializable{
     void click9(MouseEvent event) {current =9;}
     @FXML
     void click10(MouseEvent event) {current =10;}
-    
-    private TextField focusIn() {
-    	return textfield[current];
+    @FXML
+    void pressBefore(ActionEvent event) {
+    	if(textfield[current].getText().equals(""))
+    		textfield[current].setText("0");
+    	if(current ==limited) 
+    		current = 0;
+    	else
+    		current++;
+    	textfield[current].requestFocus();
+    	if(textfield[current].getText().equals("0"))
+    		textfield[current].setText("");}
+    @FXML
+    void pressNext(ActionEvent event) {
+    	if(textfield[current].getText().equals(""))
+    		textfield[current].setText("0");
+    	if(current ==0) 
+    		current = limited;
+    	else
+    		current--;
+    	textfield[current].requestFocus();
+    	if(textfield[current].getText().equals("0"))
+    		textfield[current].setText("");}
+    @FXML
+    void pressRandom(ActionEvent event) {
+    	for (int i = 0; i < limited+1; i++) {
+			textfield[i].setText(""+(int)(Math.random()*99));
+		}
     }
-
+    @FXML
+    void refreshGrade(ActionEvent event) {
+    	limited = SelecGrade.getSelectionModel().getSelectedIndex()+2;
+    	current = limited;
+    	for (int i = 0; i < textfield.length; i++) {
+    		if(i <= limited)
+    			textfield[i].setDisable(false);
+    		else {
+    			textfield[i].setText("0");
+    			textfield[i].setDisable(true);
+    		}
+		}
+    }
+    @FXML
+    void pressClean(ActionEvent event) {
+    	for (int i = 0; i < limited+1; i++) {
+			textfield[i].setText("0");
+		}
+    }
+    @FXML
+    void pressCalculate(ActionEvent event) {
+    }
+    
+    @FXML
+    void pressPost(ActionEvent event) {
+    }
+    @FXML
+    void pressPre(ActionEvent event) {
+    }
+    
+    
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
+		limited = 10;
+		current = 10;
 		textfield = new TextField[11];
 		textfield[0] = x0;
 		textfield[1] = x1;
@@ -203,6 +167,8 @@ public class CalculatorController  implements Initializable{
 		textfield[8] = x8;
 		textfield[9] = x9;
 		textfield[10] = x10;
+		SelecGrade.getItems().addAll("Grado 2", "Grado 3", "Grado 4", "Grado 5",
+				                     "Grado 6", "Grado 7", "Grado 8", "Grado 9", "Grado 10");
 	}
     
 
