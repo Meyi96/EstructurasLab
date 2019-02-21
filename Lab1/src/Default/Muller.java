bac8afcb5aa53aa57aaccd209201ed5b2c849eab
nelson quiñones <nelsonquinones2424@gmail.com>
2019-02-21 14:47:40 -0500

    Fin de documentación y analisis de algoritmos

--- Lab1/src/Default/Muller.java
+++ Lab1/src/Default/Muller.java
@@ -1,76 +1,75 @@
 package Default;
-import java.text.*;
 
-/* This is a Java implementation of Muller's algorithm for root finding,
-   which was given in Chapter 1 of your Numerical Analysis textbook.
-   Author: Martha Kosa */
 
-class Muller{
-   static double doFunction(double x) {
-      return 3 * x + x*x*x*2 - x*x*6 + 5*x*x*x*x;
-   }
-
-   static void doMuller(double x2, double x0, double x1) {
-
-      int iterNum = 1000000;    /* how many times algorithm has been performed */
-      double f0, f1, f2, fr;  /* function values */
-      double a, b, c;     /* quadratic coefficients */
-      double h1, h2, gamma, gammaplus; /* used in computing quadratic
-                                          coefficients */
-      double disc;        /* discriminant of quadratic */
-      double r1, r2;      /* roots of quadratic */
-      double xr;          /* closest root to x0 */
-      /* to print numbers with 8 digits behind the decimal point */
-      DecimalFormat df = new DecimalFormat("0.00000000");
-      double tolerance = 0.0000001;   /* indicates smallest possible interval
-                                        width to search */
-      int maxIterations = 50;  /* the maximum number of times to do
-                                  the bisection */
-
-      System.out.println("Iteration #\tX0\t\tX1\t\tX2\t\tF(X2)"); // \t is a tab
-
-      do {
-	 f2 = doFunction(x2);
-	 f0 = doFunction(x0);
-	 f1 = doFunction(x1);
-
-	 System.out.println(iterNum + "\t\t" + df.format(x0) + "\t" +
-	                    df.format(x1) + "\t" + df.format(x2) + "\t" +
-			    df.format(f2));
-
-	 h1 = x1 - x0;
-	 h2 = x0 - x2;
-	 c = f0;
-	 gamma = h2/h1;
-	 gammaplus = gamma + 1;
-	 a = (gamma * f1 - f0 * gammaplus + f2) / (gamma * h1 * h1 * gammaplus); 
-	 b = (f1 - f0 - a * h1 * h1) / h1;
-	 disc = Math.sqrt(b * b - 4 * a * c);
-	 r1 = x0 - 2 * c / (b + disc);
-	 r2 = x0 - 2 * c / (b - disc);
-
-         if (Math.abs(r1 - x0) < Math.abs(r2 - x0)) {
-	    xr = r1;
-         }
-	 else {
-	    xr = r2;
-	 }
-
-	 if (xr > x0) {
-	    x2 = x0;
-	    x0 = xr;
-	 }
-	 else {
-	    x1 = x0;
-	    x0 = xr;
-	 }
-         iterNum++;          // current iteration has been completed
-	 fr = doFunction(xr);
-      } while (Math.abs(fr) >= tolerance && iterNum <= maxIterations);
-      /* we haven't found a root and it's not time to give up yet */
-   }         
-
-   public static void main(String[] args) {
-      doMuller(0, 0.5, 1);
-   }
-}
+import java.io.*; 
+import static java.lang.Math.*; 
+  
+class Muller 
+{ 
+    static final int MAX_ITERATIONS = 10000; 
+  
+    // function to calculate f(x) 
+    static double f(double x) 
+    { 
+        // Taking f(x) = x ^ 3 + 2x ^ 2 + 10x - 20 
+        return 3*x*x +2*x - 3; 
+    } 
+  
+    static void Muller(double a, double b, double c) 
+    { 
+        int i; 
+        double res; 
+  
+        for (i = 0;; ++i) 
+        { 
+            // Calculating various constants required 
+            // to calculate x3 
+            double f1 = f(a); 
+            double f2 = f(b); 
+            double f3 = f(c); 
+            double d1 = f1 - f3; 
+            double d2 = f2 - f3; 
+            double h1 = a - c; 
+            double h2 = b - c; 
+            double a0 = f3; 
+            double a1 = (((d2*pow(h1, 2)) - (d1*pow(h2, 2))) 
+                        / ((h1*h2) * (h1-h2))); 
+            double a2 = (((d1*h2) - (d2*h1))/((h1*h2) * (h1-h2))); 
+            double x = ((-2*a0)/(a1 + abs(sqrt(a1*a1-4*a0*a2)))); 
+            double y = ((-2*a0)/(a1-abs(sqrt(a1*a1-4*a0*a2)))); 
+  
+            // Taking the root which is closer to x2 
+            if (x >= y) 
+                res = x + c; 
+            else
+                res = y + c; 
+  
+            // checking for resemblance of x3 with x2 till 
+            // two decimal places 
+            double m = res*100; 
+            double n = c*100; 
+            m = floor(m); 
+            n = floor(n); 
+            if (m == n) 
+                break; 
+            a = b; 
+            b = c; 
+            c = res; 
+            if (i > MAX_ITERATIONS) 
+            { 
+                System.out.println("Root cannot be found using" + 
+                                   " Muller's method"); 
+                break; 
+            } 
+        } 
+        if (i <= MAX_ITERATIONS) 
+            System.out.println("The value of the root is " + res); 
+    } 
+  
+    // Driver main function 
+    public static void main(String args[]) 
+    { 
+        double a = 0, b = 1, c = 2; 
+        Muller(a, b, c); 
+    } 
+} 
